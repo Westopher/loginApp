838775418d86e5361365c7d175b2f6fabb2c6f8b
West Kraemer <kraemerwest@gmail.com>
2019-03-19 12:20:35 -0400

    set nav bar title to username on clcick of login

--- login/ViewController.swift
+++ login/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameTextfield: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = usernameTextfield.text
+    }
 
 }
 
